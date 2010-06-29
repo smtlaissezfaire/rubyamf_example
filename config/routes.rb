ceb73f4a239372dea50debfc09357830585f073c
Scott Taylor <scott@railsnewbie.com>
2010-06-29 11:37:47 -0400

    ruby script/generate scaffold Message text:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :messages
+
   map.rubyamf_gateway 'rubyamf_gateway', :controller => 'rubyamf', :action => 'gateway'
 
   # The priority is based upon order of creation: first created -> highest priority.
