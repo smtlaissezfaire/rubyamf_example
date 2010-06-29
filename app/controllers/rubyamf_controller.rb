827556394e072ccf49f2d0aed032fd3fd06f609a
Scott Taylor <scott@railsnewbie.com>
2010-06-29 11:36:50 -0400

    whitespace cleanup

--- app/controllers/rubyamf_controller.rb
+++ app/controllers/rubyamf_controller.rb
@@ -1,20 +1,19 @@
 class RubyamfController < ActionController::Base
-
   include RubyAMF::App
-  
-  def gateway      
+
+  def gateway
     RequestStore.rails_authentication = nil #clear auth hash
     RequestStore.rails_request = request
     RequestStore.rails_response = response
-          
+
     #Compress the amf output for smaller data transfer over the wire
     RequestStore.gzip = request.env['ACCEPT_ENCODING'].to_s.match(/gzip,[\s]{0,1}deflate/)
-    
+
     #if not flash user agent, send some html content
-    amf_response = if request.env['CONTENT_TYPE'].to_s.match(/x-amf/) 
+    amf_response = if request.env['CONTENT_TYPE'].to_s.match(/x-amf/)
         headers['Content-Type'] = "application/x-amf"
         RailsGateway.new.service(request.raw_post) #send the raw data throught the rubyamf gateway and create the response
-      else 
+      else
         headers['Content-Type'] = "text/html"
         welcome_screen_html # load in some stub html
       end
@@ -22,15 +21,14 @@ class RubyamfController < ActionController::Base
     # this is a fugly patch, but I'm not sure what the real problem is. Content-Type is set for gateway requests
     # and type is set for other requests.
     headers['Content-Type'] ||= headers['type']
-            
+
     #render the AMF
     send_data(amf_response, {:type => headers['Content-Type'], :disposition => 'inline'})
   rescue Exception => e #only errors in this scope will ever be rescued here, see BatchFiler
     STDOUT.puts e.to_s
     STDOUT.puts e.backtrace
   end
-  
-  
+
   def rescue_action(e)
     #There are a couple things that will trigger this rescue_action. Which aren't
     #ever returned to the flash player. be ware. but I will put a fix for this in.
@@ -38,8 +36,9 @@ class RubyamfController < ActionController::Base
     puts e.message
     puts e.backtrace
   end
-  
-  private
+
+private
+
   def welcome_screen_html
     "<html>
       <head>
@@ -50,5 +49,4 @@ class RubyamfController < ActionController::Base
       <table width='100%' align=center valign=middle height='100%'><tr><td width=100 align=center>
         <a href='http://blog.rubyamf.org'><img border=0 src='http://blog.rubyamf.org/images/gateway.png' /></a>"
   end
-  
 end
--- config/rubyamf_config.rb
+++ config/rubyamf_config.rb
@@ -1,38 +1,39 @@
 require 'app/configuration'
+
 module RubyAMF
   module Configuration
     #set the service path used in all requests
     # RubyAMF::App::RequestStore.service_path = File.expand_path(RAILS_ROOT) + '/app/controllers'
 
     # => CLASS MAPPING CONFIGURATION
-    
+
     # => Global Property Ignoring
     # By putting attribute names into this array, you opt in to globally ignore these properties on incoming objects.
-    # If you want to ignore specific properties on certain objects, use the :ignore_fields property in a 
+    # If you want to ignore specific properties on certain objects, use the :ignore_fields property in a
     # Class Mapping definition (see CLASS MAPPING DEFINITIONS)
     # ClassMappings.ignore_fields = ['created_at','created_on','updated_at','updated_on']
-  
+
     # => Case Translations
     # Most actionscript uses camelCase instead of snake_case. Set ClassMappings.translate_case to true if want translations to occur.
     # The translations only occur on object properties
     # An incoming property like: myProperty gets turned into my_property
     # An outgoing property like my_property gets turned into myProperty
     #ClassMappings.translate_case = false
-  
+
     # => Force Active Record Ids
     # includes the id field for activerecord objects even if you don't specify it when using custom attributes. This is important for deserialization
     # where ids are needed to keep active record association integrity.
     # ClassMappings.force_active_record_ids = true
-    
+
     # => Hash key access
-    # You can choose how keys in hashes are created. As :string, :symbol, or :indifferent. 
+    # You can choose how keys in hashes are created. As :string, :symbol, or :indifferent.
     # :string creates keys as hash['key']
     # :symbol creates keys as hash[:key]
     # :indifferent uses rails HashWithIndifferentAccess so you can use hash[:key] of hash['key']
     # There are performance issues with HashWithIndifferentAccess. Use :symbol or :string for best performance.
     # The default is :symbol
     # ClassMappings.hash_key_access = :symbol
-    
+
     # => Assume Class Types
     # This tells RubyAMF to assume class type transfers. So when you register a class Alias from Flash or Flex like this:
     # Flash::   fl.net.registerClassAlias('User',User)
@@ -40,7 +41,7 @@ module RubyAMF
     # RubyAMF will automagically convert it to a User active record without you having to create a class mapping.
     # This also works with non active record class mappings. See the wiki on the google code page for a downloadable example.
     # ClassMappings.assume_types = false
-    
+
     # => Class Mapping Definitions
     # A Class Mapping definition conists of at least these two properties:
     # :actionscript   # The incoming action script class to watch for
@@ -53,7 +54,7 @@ module RubyAMF
     # :methods        # An array of methods to call and place values in a similarly named attribute on the Actionscript Object (outgoing, or Rails => Actionscript only)
     # :ignore_fields  # An array of field names you want to ignore on incoming classes
     #
-    # If you are using ActiveRecord VO's you do not need to specify a fully qualified class path to the model, you can just define the class name, 
+    # If you are using ActiveRecord VO's you do not need to specify a fully qualified class path to the model, you can just define the class name,
     # EX: ClassMappings.register(:actionscript => 'vo.Person', :ruby => 'Person', :type => 'active_record')
     #
     # If you are using custom VO's you would need to specify the fully qualified class path to the file
@@ -69,35 +70,35 @@ module RubyAMF
     # in flex as opposed to editing a book (where you would need more parameters). You can define a scope mapping parameter for ":attributes"
     # or for ":associations." You're mapping would look something like this.
     # ClassMappings.register(
-    #   :actionscript  => 'com.mixbook.vo.books.BookVO',      
-    #   :ruby          => 'Book',      
+    #   :actionscript  => 'com.mixbook.vo.books.BookVO',
+    #   :ruby          => 'Book',
     #   :type          => 'active_record',
-    #   :associations  => ["access_info", "pages", "page_ratio"],  
+    #   :associations  => ["access_info", "pages", "page_ratio"],
     #   :attributes    => {:viewing => ["description", "title"], :editing => ["id","published_at","theme_id"] }    <=== notice the hash instead of an array
     #
     # Now, to call the class mapping scope of editing (you are sending objects to the editing application), your controller call would look like this:
     # EX: render :amf => book, :class_mapping_scope => :editing
     #
-    # You can also specify a default scope to use. If you don't set this and you don't specify a class mapping scope on an attribute or association, then 
+    # You can also specify a default scope to use. If you don't set this and you don't specify a class mapping scope on an attribute or association, then
     # it will not have a scope to use and will not add any attributes or associations (whichever it cant match) to that association.
     # ClassMappings.default_mapping_scope = :viewing
-  
+
     # => Date Conversion
     # Incoming dates from Flash by default are Time objects, this can conver to DateTime if needed
     # ClassMappings.use_ruby_date_time = false
-  
+
     # => Use Array Collection
     # By setting this to true, you opt in to using array collections for all the arrays generated by the body of your request.
     # Note: This only works for amf3 with Remote Object, NOT with Net Connection.
     # ClassMappings.use_array_collection = false
-  
+
     # => Check for Associations
     # Enabling this will automagically pick up eager loaded association data on objects returned through RubyAMF.
     # If this is disabled, you will need to specify any associations you DO want picked up in the ClassMapping
     # ClassMappings.check_for_associations = true
-    
+
     # => NAMED PARAMETER MAPPING CONFIGURATION
-    
+
     #=> Always Put Remoting Parameters into the "params" hash
     # If set to true, arguments from Flash/Flex will come in to the controllers as params[0], params[1], etc.. This is especally useful if you are sending huge objects
     # from Flex into Ruby so it doesnt eat up all your output window with outputting the params in the controller/action header information while in dev mode.
@@ -105,11 +106,11 @@ module RubyAMF
     # always have access to the parameters from rubyamf in your controller by calling rubyamf_params[0], rubyamf_params[1], etc regardless of
     # if it this is set or not.
     # ParameterMappings.always_add_to_params = true
-     
+
     # => Return Top Level Hash
     # For those scaffolding users out there, who want the top-level object to come as a hash so scaffolding works out of the box.
     # ParameterMappings.scaffolding = false
-  
+
     # => Incoming Remoting Parameter Mappings
     # Incoming Remoting Parameter mappings allow you to map an incoming requests parameters into rails' params hash
     #
